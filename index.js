3f0157906c94ad4cfb142fecd59bebcd3491da9f
Jacob Kanfer <jacobkanfer@jacobs-mbp.lan>
2020-02-12 05:04:58 -0500

    Continued work on policy page

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function toggleDropdown() {
-  document.getElementById('candidateNav').classList.toggle('show');
+  document.getElementById('searchBox').classList.toggle('show');
 }
 
 function filterFunction() {
   var input, filter, ul, li, a, i;
-  input = document.getElementById('allCandidates');
+  input = document.getElementById('content');
   filter = input.value.toUpperCase();
-  div = document.getElementById('candidateNav');
+  div = document.getElementById('searchBox');
   a = div.getElementsByTagName('a');
   for (i = 0; i < a.length; i++) {
     txtValue = a[i].textContent || a[i].innerText;
@@ -15,5 +15,21 @@ function filterFunction() {
     }else {
       a[i].style.display = 'none';
     }
+  }
+}
+
+function readMore(dotNum, moreNum, seeMoreNum) {
+  var dots = document.getElementById(dotNum);
+  var moreText = document.getElementById(moreNum);
+  var btnText = document.getElementById(seeMoreNum);
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
   }
 }
